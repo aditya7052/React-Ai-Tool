c3785dde9eff486852ba593192d5faee398e9808
aditya7052 <adityakumarssm16@gmail.com>
2025-09-06 23:46:46 +0530

    next step

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { checkHeading, replaceHeadingStarts } from '../helper'
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import style from 'react-syntax-highlighter/dist/esm/styles/hljs/a11y-dark';
+import ReactMarkdown from 'react-markdown'
+import SyntaxHighligher from 'react-syntax-highlighter'
 
 const Answers = ({ ans,totalResult, index, type }) => {
     const [heading, setHeading] = useState(false)
@@ -12,12 +16,29 @@ const Answers = ({ ans,totalResult, index, type }) => {
         }
     }, [])
 
-
+    const renderer ={
+        code({node, inline, className, children,...props}){
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ?(
+                <SyntaxHighligher 
+                {...props}
+                children={String(children).replace(/\n$/,'')}
+                language={match[1]}
+                style={dark}
+                preTag = "div"
+                />
+            ):(
+                <code {...props} className={className}>
+                    {children}
+                </code>
+            )
+        }
+    }
     return (
         <>
         {
             index == 0 && totalResult >1?<span className='text-xl'>{answer}</span>:heading ? <span className={"pt-2 block text-white"}>{answer}</span> :
-             <span className={type=='q'? 'pl-1':'pl-5'}>{answer}</span>
+             <span className={type=='q'? 'pl-1':'pl-5'}><ReactMarkdown components={renderer} >{answer}</ReactMarkdown> </span>
         }
             
         </>
